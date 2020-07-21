1fdd2f83fc4a086b425d762b03318ab1857edf4a
Johan-Mi <johanmilanov@outlook.com>
2020-07-21 19:36:28 +0200

    -

--- config.h
+++ config.h
@@ -1,20 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"noto sans:size=11",
+	"fira mono:size=11",
 	"JoyPixels:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#eeeeee", "#0c0c0c" },
-	[SchemeSel] = { "#0c0c0c", "#01a0e4" },
-	[SchemeSelHighlight] = { "#eeeeee", "#01a0e4" },
-	[SchemeNormHighlight] = { "#01a0e4", "#0c0c0c" },
-	[SchemeOut] = { "#0c0c0c", "#00ffff" },
+	[SchemeNorm] = { "#bbc2cf", "#0e0e0e" },
+	[SchemeSel] = { "#bbc2cf", "#2257a0" },
+	[SchemeSelHighlight] = { "#C678DD", "#091633" },
+	[SchemeNormHighlight] = { "#C678DD", "#1C1F24" },
+	[SchemeOut] = { "#0e0e0e", "#00ffff" },
+	[SchemeCursor] = { "#51afef", "#0e0e0e" },
+	[SchemePrompt] = { "#51afef", "#0e0e0e" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
